04dc73eb36bd17a961433c5aeb37a4252f9d1cde
diegodovale123 <165052962+diegodovale123@users.noreply.github.com>
2024-04-09 12:28:44 -0300

    Update exe1.py

--- trabalho/primeiro_trimestre/lista_prova/exe1.py
+++ trabalho/primeiro_trimestre/lista_prova/exe1.py
@@ -1,1 +1,31 @@
+def calcular_imc(peso, altura):
+   
+    imc = peso / (altura ** 2)
+    return imc
 
+def classificar_imc(imc):
+  
+    if imc < 18.5:
+        return "Abaixo do peso"
+    elif imc < 25:
+        return "Peso normal"
+    elif imc < 30:
+        return "Sobrepeso"
+    else:
+        return "Obesidade"
+
+# Função principal
+def main():
+    print("Calculadora de Índice de Massa Corporal (IMC)")
+    print("--------------------------------------------")
+    peso = float(input("Digite o seu peso em kg: "))
+    altura = float(input("Digite a sua altura em metros: "))
+
+    imc = calcular_imc(peso, altura)
+    classificacao = classificar_imc(imc)
+
+    print("\nSeu IMC é:", imc)
+    print("Classificação:", classificacao)
+
+if __name__ == "__main__":
+    main()
