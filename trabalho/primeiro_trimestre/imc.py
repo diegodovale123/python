784baf9a4f99c3e97b466cd24d2a790b2256c42d
diegodovale123 <165052962+diegodovale123@users.noreply.github.com>
2024-04-10 11:32:11 -0300

    Update imc.py

--- trabalho/primeiro_trimestre/imc.py
+++ trabalho/primeiro_trimestre/imc.py
@@ -1,31 +1,24 @@
-def calcular_imc(peso, altura):
+x = float(input("Digite o seu peso em quilos: "))
+y = float(input("DIgite a sua altura em metros: "))
+
+imc = x/(y**2)
+print(f"{imc:.2f}")
+
+if imc<18.5:
+    print("Você está abaixo do peso.")
+elif imc>=18.5 and imc<25.0:
+    print("Seu peso está normal.")
+elif imc>=25.0 and imc<30.0:
+    print("Você está com sobrepeso.")
+elif imc>=30.0 and imc<35.0:
+    perda=x*0.2
+    print("Seu peso está com obesidade em grau 1, precisa perder cerca de ", perda, "quilos.")
+elif imc>=35.0 and imc<40.0:
+    perda=x*0.3
+    print("Seu peso está com obesidade em grau 2, precisa perder cerca de ", perda, "quilos.")
+else:
+    perda=x*0.4
+    print("Seu peso está com obesidade em grau 3, precisa perder cerca de ", perda, "quilos.")
+
    
-    imc = peso / (altura ** 2)
-    return imc
-
-def classificar_imc(imc):
-  
-    if imc < 18.5:
-        return "Abaixo do peso"
-    elif imc < 25:
-        return "Peso normal"
-    elif imc < 30:
-        return "Sobrepeso"
-    else:
-        return "Obesidade"
-
-# Função principal
-def main():
-    print("Calculadora de Índice de Massa Corporal (IMC)")
-    print("--------------------------------------------")
-    peso = float(input("Digite o seu peso em kg: "))
-    altura = float(input("Digite a sua altura em metros: "))
-
-    imc = calcular_imc(peso, altura)
-    classificacao = classificar_imc(imc)
-
-    print("\nSeu IMC é:", imc)
-    print("Classificação:", classificacao)
-
-if __name__ == "__main__":
-    main()
+ 
